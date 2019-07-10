671b77e6082a8698f72cd6bd6a500a296534000b
chetanwayade <chetanwayade@gmail.com>
2019-07-10 15:11:11 +0530

    zipcode changes

--- src/components/AddressDetails.js
+++ src/components/AddressDetails.js
@@ -1,36 +1,46 @@
-import React from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { CardHeader } from '../../node_modules/@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import ClearIcon from '@material-ui/icons/Clear';
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { CardHeader } from "../../node_modules/@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import ClearIcon from "@material-ui/icons/Clear";
 
-const AddressDetails = ({handleClick, address}) => {     
-    const regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
-    
-    return (
-    <Drawer anchor="right" open={true} onClose={() => handleClick(false)}>                
-        <Card>
-            <CardHeader title="Address Details " action={
-            <IconButton aria-label="Settings" onClick={() => handleClick(false)}>   
-                <ClearIcon />      
+const AddressDetails = ({ handleClick, address, validateZipcode }) => {
+  return (
+    <Drawer anchor="right" open={true} onClose={() => handleClick(false)}>
+      <Card>
+        <CardHeader
+          title="Address Details "
+          action={
+            <IconButton
+              aria-label="Settings"
+              onClick={() => handleClick(false)}
+            >
+              <ClearIcon />
             </IconButton>
-            }/>
-            <CardContent>
-            <Typography color="textSecondary">
-                <strong>{address.name}</strong>
-            </Typography>
-            <Typography color="textSecondary">
-                <strong>
-                { address.address.street + ', ' + address.city + (regex.test(address.address.zipcode) ? (', ' + address.address.zipcode) : '.')}
-                </strong>
-            </Typography>
-            </CardContent>
-        </Card>
-      </Drawer>        
-    );
-}
+          }
+        />
+        <CardContent>
+          <Typography color="textSecondary">
+            <strong>{address.name}</strong>
+          </Typography>
+          <Typography color="textSecondary">
+            <strong>
+              {validateZipcode(address.address.zipcode)
+                ? address.address.street +
+                  ", " +
+                  address.address.city +
+                  ", " +
+                  address.address.zipcode
+                : address.address.street + ", " + address.address.city + "."}
+            </strong>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Drawer>
+  );
+};
 
 export default AddressDetails;
--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,41 +1,40 @@
-import React, { Fragment } from 'react';
-import { createStyles, withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import '../css/Table.css';
-import AddressDetails from './AddressDetails';
+import React, { Fragment } from "react";
+import { createStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import "../css/Table.css";
+import AddressDetails from "./AddressDetails";
 
-const StyledTableCell = withStyles((theme) =>
+const StyledTableCell = withStyles(theme =>
   createStyles({
     head: {
       backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
+      color: theme.palette.common.white
     },
     body: {
-      fontSize: 14,
-    },
-  }),
+      fontSize: 14
+    }
+  })
 )(TableCell);
 
-const UserTable = props => {
-  const regex = /^[0-9]{5}(?:-[0-9]{4})?$/;
-  const rows = props.users;
+const UserTable = ({ users, validateZipcode }) => {
+  const rows = users;
   const [state, setState] = React.useState({
     right: false,
     selecteRow: {
       address: {
         street: null,
         city: null,
-        zipcode: '',
+        zipcode: ""
       }
     }
   });
 
-  const handleClick = (open, row) => {    
+  const handleClick = (open, row) => {
     if (open) {
       setState({
         selecteRow: row,
@@ -46,16 +45,18 @@ const UserTable = props => {
         right: false
       });
     }
-  }      
+  };
 
   return (
     <Fragment>
-      <h2 className='userTitle'>User Details</h2>
+      <h2 className="userTitle">User Details</h2>
       <Paper>
         <Table>
           <TableHead>
             <TableRow>
-              <StyledTableCell component="th" scope="row">Id</StyledTableCell>
+              <StyledTableCell component="th" scope="row">
+                Id
+              </StyledTableCell>
               <StyledTableCell align="right">Name</StyledTableCell>
               <StyledTableCell align="right">Username</StyledTableCell>
               <StyledTableCell align="right">Email</StyledTableCell>
@@ -64,24 +65,42 @@ const UserTable = props => {
           </TableHead>
           <TableBody>
             {rows.map(row => (
-              <TableRow tabIndex={row.id} key={row.id} className={regex.test(row.address.zipcode) ? '' : 'highlight'}>
+              <TableRow
+                tabIndex={row.id}
+                key={row.id}
+                className={
+                  validateZipcode(row.address.zipcode) ? "" : "highlight"
+                }
+              >
                 <StyledTableCell component="th" scope="row">
                   {row.id}
                 </StyledTableCell>
                 <StyledTableCell align="right">{row.name}</StyledTableCell>
                 <StyledTableCell align="right">{row.username}</StyledTableCell>
                 <StyledTableCell align="right">{row.email}</StyledTableCell>
-                <StyledTableCell align="right" onClick={() => handleClick(true, row)} className='handCursor'>
-                  {row.address.street + ', ' + row.address.city}
+                <StyledTableCell
+                  align="right"
+                  onClick={() => handleClick(true, row)}
+                  className="handCursor"
+                >
+                  {row.address.street + ", " + row.address.city}
                 </StyledTableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </Paper>
-      {state.right ? <AddressDetails address={state.selecteRow} handleClick={handleClick}></AddressDetails> : ''}      
+      {state.right ? (
+        <AddressDetails
+          address={state.selecteRow}
+          handleClick={handleClick}
+          validateZipcode={validateZipcode}
+        />
+      ) : (
+        ""
+      )}
     </Fragment>
-  )
-}
+  );
+};
 
 export default UserTable;
